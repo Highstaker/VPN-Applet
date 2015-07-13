fa4a47f778bb5134a91cbceedb96e2f09307f0d1
Highstaker <heights999@yandex.ru>
2015-07-13 10:02:16 +0300

    v1.5.0
    +added notification toggle
    +added data refresh button
    +better management of IP-getting processes
    +minor fixes

--- applet_myip.py
+++ applet_myip.py
@@ -11,17 +11,28 @@ from country_codes import COUNTRIES
 from threading import Thread, Timer, Lock
 import socket
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import signal
 
 CONFIG_FILES_PATH="/home/highstaker/Документы/ibvpn_openvpn/"
 SIGNAL_FILE_PATH="/tmp/"
 SIGNAL_FILENAME="openVPN_aliver_command.txt"
+
+def exMsg(message):
+	'''
+	Shows a message tagged as [EXCEPTION]
+	'''
+	print("[EXCEPTION]" + message)
+
 
 class menuRefresher:
 
 	lock1 = Lock()
 
 	#how often, in seconds, we need to refresh the IP and indicator menus
-	REFRESH_TIMEOUT = 5
+	REFRESH_TIMEOUT = 10
+
+	#A timeout for IP-getting process. If it doesn't get IP within that time - it returns none and the subprocess is destroyed.
+	GETTER_TIMEOUT = 5
 
 	#handle to the indicator
 	hIndicator = None
@@ -32,8 +43,14 @@ class menuRefresher:
 	#a flag, whether to show label with IP in tray or not
 	show_label=False
 
+	#a flag, whether to show notifications or not
+	show_notifications=True
+
 	def change_server(self,widget,config_filename):
-		pass
+		'''
+		Invoked when the menu entry for server change or VPN stop is pressed
+		Creates a signal file for the daemon
+		'''
 		print("[DEBUG]Change server to " + config_filename)
 		print("[DEBUG]Signal file: " + SIGNAL_FILE_PATH + SIGNAL_FILENAME)
 
@@ -44,8 +61,15 @@ class menuRefresher:
 		return True
 
 	def get_country(self,ip):
+		'''
+		Returns the country code of the country the IP is based in
+		'''
 
-		getter_process = subprocess.Popen(["whois", ip],stdout=subprocess.PIPE)
+		try:
+			getter_process = subprocess.Popen(["whois", ip],stdout=subprocess.PIPE)
+		except:
+			exMsg("Could not get country. Is 'whois' installed?")
+			return ""
 
 		ipCountry = getter_process.communicate()[0].decode(encoding="ascii", errors="ignore")
 		ipCountry = [line for line in ipCountry.split("\n") if (("country" in line) or ("Country" in line))]    
@@ -55,10 +79,14 @@ class menuRefresher:
 
 		ipCountry = ipCountry[0]
 		ipCountry = ipCountry.replace(" ","").replace("\t","").replace("country:","").replace("Country:","")
+		ipCountry = ipCountry.upper()
 
 		return ipCountry
 
 	def refresh_label(self,ind,myip):
+		'''
+		Shows the label near the applet icon in the top bar, but only if show_label flag is True.
+		'''
 		if self.show_label:
 			ind.set_label(myip,"")
 		else:
@@ -68,9 +96,30 @@ class menuRefresher:
 		return True
 
 	def toggle_label(self,widget,ind,myip):
+		'''
+		Toggles the label in top bar by changing the show_label flag.
+		'''
 		self.show_label= not self.show_label
 		self.refresh_label(ind,myip)
 
+		return True
+
+	def toggle_notifications(self,widget,ind,myip):
+		'''
+		Toggles the notifications by changing the show_notifications flag.
+		'''
+		self.show_notifications= not self.show_notifications
+
+		return True
+
+	def refresh_data(self,widget):
+		'''
+		Re-ask the IP data manually.
+		Need to clear buffer to force applet to update.
+		'''
+		self.IP_buf = None
+		self.askIP(self.hIndicator)
+		
 		return True
 
 	def setIndicatingMenus(self,myip,ind):
@@ -84,15 +133,21 @@ class menuRefresher:
 		else:
 			self.IP_buf = myip
 
+		if not myip:
+			myip = ""
+
 		print("[DEBUG]Getting country")
-		myCountry = self.get_country(myip) #country CODE
-		# myCountry = "BD" #debug
-		myCountryName = [i for i in COUNTRIES if myCountry in i] #name of a country
+		myCountry = ""
+		if myip:
+			myCountry = self.get_country(myip) #country CODE
+		if myCountry:
+			myCountryName = [i for i in COUNTRIES if myCountry in i] #name of a country
+		else:
+			myCountryName = [["Unknown"]]
 		if myCountryName:
 			myCountryName = myCountryName[0][0]
 			flagIconFilename = "Flag_of_" + myCountryName.replace(" ","_")
 		else:
-			print("[DEBUG]Could not get country name")
 			flagIconFilename = ""
 			myCountryName = ""
 
@@ -113,6 +168,8 @@ class menuRefresher:
 		menu.append(menu_items)        
 		menu_items.show()
 
+		# menu.append(Gtk.SeparatorMenuItem())
+
 		menu_items = Gtk.MenuItem("-"*20)
 		menu.append(menu_items)        
 		menu_items.show()
@@ -127,10 +184,22 @@ class menuRefresher:
 		menu.append(menu_items)        
 		menu_items.show()
 
+		menu_items = Gtk.MenuItem( "Toggle Notifications" )
+		menu_items.connect("activate",self.toggle_notifications,ind,myip)
+		menu.append(menu_items)        
+		menu_items.show()
+
+		menu_items = Gtk.MenuItem( "Refresh Data" )
+		menu_items.connect("activate",self.refresh_data)
+		menu.append(menu_items)        
+		menu_items.show()
+
 		menu_items = Gtk.MenuItem("Exit") 
 		menu_items.connect("activate",Gtk.main_quit)
 		menu.append(menu_items)        
 		menu_items.show()
+
+		# menu.append(Gtk.SeparatorMenuItem())
 
 		menu_items = Gtk.MenuItem("-"*20)
 		menu.append(menu_items)        
@@ -152,8 +221,17 @@ class menuRefresher:
 
 		ind.set_icon_full(flagIconFilename,myCountry)
 
-		subprocess.Popen(['notify-send', "Your external IP has changed", \
-			"New IP: " + myip + "\nCountry: " + myCountryName])
+		if not myip:
+			notification_message = "Could not get IP. Connection lost."
+		else:
+			notification_message = "Your external IP has changed"
+
+		if self.show_notifications:
+			try:
+				subprocess.Popen(['notify-send', notification_message, \
+				"New IP: " + myip + "\nCountry: " + myCountryName])
+			except:
+				exMsg("Could not send notification. Is notify-send available in this system?")
 
 		GLib.idle_add(self.startRefreshSequence,ind)
 
@@ -164,13 +242,20 @@ class menuRefresher:
 		"""
 		A thread that sets an IP using the method specified in commandline
 		"""
-
-		getter_process = subprocess.Popen(commandline,stdout=subprocess.PIPE)
-		myip = getter_process.communicate()[0].decode(encoding="ascii", errors="ignore").replace("\n","")
-
-		print("getIP with ", commandline, " result:" , myip)
-
-		return myip if isIP(myip) else None
+		try:
+			getter_process = subprocess.Popen(commandline,stdout=subprocess.PIPE)
+		except:
+			print("Could not get IP with this method. Is ", commandline[0], " installed?")
+			return None
+		try:
+			myip = getter_process.communicate(timeout=self.GETTER_TIMEOUT)[0].decode(encoding="ascii", errors="ignore").replace("\n","")
+		except subprocess.TimeoutExpired:
+			getter_process.kill()
+			print("IP getter with command line ", commandline, "timed out")
+			return None
+		else:
+			print("getIP with ", commandline, " result:" , myip)
+			return myip if isIP(myip) else None
 
 
 	def askIP(self,ind):
@@ -189,11 +274,15 @@ class menuRefresher:
 		def controller_getIP():
 			with ThreadPoolExecutor(len(IPgettingMethods)) as executor:
 				futures = [executor.submit(self.getIP, i) for i in IPgettingMethods]
-				for future in as_completed(futures):
+				for future in as_completed(futures):					
 					ip = future.result()
 					if ip:
 						GLib.idle_add(self.setIndicatingMenus, ip, self.hIndicator)
 						break
+				else:
+					#could not get IP by any means. No connection?
+					GLib.idle_add(self.setIndicatingMenus, None, self.hIndicator)
+
 
 
 		th = Thread(target=controller_getIP)
@@ -234,6 +323,9 @@ def isIP(check):
 	return True
 
 def main():
+
+	# Catch CTRL-C
+	signal.signal(signal.SIGINT, lambda signal, frame: Gtk.main_quit())
 
 	GObject.threads_init()
 
